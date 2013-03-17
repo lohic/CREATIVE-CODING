7add1c583316dfa0873390fa81232b8e8a3e1655
Loïc Horellou <loic@formidable-studio.net>
2013-03-17 20:46:01 +0100

    update link for ref

--- RASPBERRY/python/video_button_loic.py
+++ RASPBERRY/python/video_button_loic.py
@@ -46,3 +46,17 @@ def main():
 
 #---- ON LANCE LE SERVEUR
 main()
+
+
+# ** http://opensoundcontrol.org/implementation/python-simple-osc
+# ** http://www.ixi-audio.net/content/body_backyard_python.html
+# http://wiki.labomedia.org/index.php/Envoyer_et_recevoir_de_l'OSC_en_python
+# https://trac.v2.nl/wiki/pyOSC
+# http://www.brianhensley.net/2012/07/how-to-get-1080p-videos-running-on-my.html
+# http://elinux.org/Omxplayer
+# ** http://hertaville.com/2012/11/18/introduction-to-accessing-the-raspberry-pis-gpio-in-c/
+# http://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/robot/buttons_and_switches/
+# http://learn.adafruit.com/playing-sounds-and-using-buttons-with-raspberry-pi/code
+# ** http://learn.adafruit.com/adafruit-pi-cobbler-kit/overview#
+# ** http://niltoid.com/blog/raspberry-pi-arduino/
+# http://www.vrplumber.com/py3d.py
